ba42f9c363fcebdac3abf855419cc795c30b5425
Fahd Alrashedi <ifahd.dev@gmail.com>
2020-10-17 20:59:28 +0300

    update auth.py environ

--- backend/auth.py
+++ backend/auth.py
@@ -7,9 +7,9 @@ from urllib.request import urlopen
 
 # Static data
 # ------------------------------------------------------
-AUTH0_DOMAIN = os.environ['AUTH0_DOMAIN']
-ALGORITHMS = os.environ['ALGORITHMS']
-API_AUDIENCE = os.environ['API_AUDIENCE']
+AUTH0_DOMAIN = os.environ.get('AUTH0_DOMAIN')
+ALGORITHMS = os.environ.get('ALGORITHMS')
+API_AUDIENCE = os.environ.get('API_AUDIENCE')
 
 # AuthError Exception
 # ------------------------------------------------------
